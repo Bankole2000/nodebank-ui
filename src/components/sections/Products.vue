06c14ee6beebff73a17f8a0ca269be63f3717fef
Bankole <ebankole@aidml.com>
2022-03-23 17:33:30 +0100

    300k

--- src/components/sections/Products.vue
+++ src/components/sections/Products.vue
@@ -25,7 +25,7 @@
         </v-container>
         <div v-else class="ma-4">
           <v-card elevation="0" class="py-8 elevated-light">
-            <h2 class="text-center px-4 mb-4">We've created products that are solving problems for over 50,000+ people</h2>
+            <h2 class="text-center px-4 mb-4">We've created products that are solving problems for over 300,000+ people</h2>
             <div class="text-center d-flex align-center justify-center" v-for="i in 5" :key="i">
                       <v-img class="mt-4 mb-4" :src="require('@/assets/icons/nodcheckoutlogo.svg')" max-width="220px"></v-img>
                     </div>
