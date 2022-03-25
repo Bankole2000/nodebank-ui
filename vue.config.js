95482e47e43c29ef4b9d9a061468a96aba0f2a2c
Bankole <ebankole@aidml.com>
2022-03-25 14:18:39 +0100

    fixing routing

--- vue.config.js
+++ vue.config.js
@@ -7,7 +7,8 @@ module.exports = {
     });
   },
   // publicPath: process.env.NODE_ENV === "production" ? "/webpieces/" : "/"
-  publicPath: process.env.NODE_ENV === "production" ? "/nodebank-ui" : "/"
+  // publicPath: process.env.NODE_ENV === "production" ? "/nodebank-ui" : "/"
+  publicPath: process.env.NODE_ENV === "production" ? "/" : "/"
   // publicPath: '',
   // baseUrl: '/projects/vue/webpieces/dist/',
 };
